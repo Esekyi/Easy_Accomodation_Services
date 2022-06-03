b38d4419612e84c372cec87a9cbb76910c9f3b69
[esekyi] <[sskert10@gmail.com]>
2022-06-03 03:45:31 +0000

    🔥[Update]Added webview of WebApp

--- Screens/ViewScreen.js
+++ Screens/ViewScreen.js
@@ -1,31 +1,57 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Button } from 'react-native-paper';
+import { SafeAreaView, StyleSheet, ActivityIndicator, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const ViewScreen = () =>
+const MainScreen = () =>
 {
-    const [title, setTitle] = useState("")
-    const [price, setPrice] = useState("")
+    const [visible, setVisible] = useState(false);
+    const ActivityIndicatorElement = () =>
+    {
+        return (
+            <View style = {styles.ActivityIndicatorStyling}>
+                <ActivityIndicator color= 'gray' size="large" />
+            </View>
+        )
+    }
     return (
-        <View style={styles.container}>
-            <SafeAreaView>
 
-            <Text>My New Home</Text>
-            </SafeAreaView>
-        </View>
+        <WebView
+            useWebKit={true}
+            source=
+            {{ uri: "https://easy-apartment14.herokuapp.com/" }}
+            // JavaScript Support
+            javaScriptEnabled={true}
+            //Cache
+            domStorageEnabled={true}
+            onLoadStart={() => setVisible(true)}
+            onLoad={() => setVisible(false)}
+            startInLoadingState={true}
+            // style={{ marginTop: 20 }}
+        />
+            
+        // {visible ? <ActivityIndicatorElement /> : null}
+
     );
-}
-
-export default ViewScreen;
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent:'center',
-        
     },
-
-}
-)
+    ActivityIndicatorStyling: {
+        flex: 1,
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: 'auto',
+        marginTop: 'auto',
+        left:'auto',
+        Right:'auto',
+        Top:'auto',
+        Bottom: 'auto',
+        justifyContent:'center',
+    }
+})
+export default MainScreen;
