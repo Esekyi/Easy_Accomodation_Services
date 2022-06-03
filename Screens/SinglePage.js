b60e721ca66d4caeb291982a2f62a2d92fb9c600
[esekyi] <[sskert10@gmail.com]>
2022-06-03 05:17:23 +0000

    [Render🏡]Displayed all data fetched

--- Screens/SinglePage.js
+++ Screens/SinglePage.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { View, SafeAreaView } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { View, SafeAreaView,ScrollView } from 'react-native';
+import { Divider, Button, Card, Title, Paragraph } from 'react-native-paper';
+import ViewScreen from '../Screens/ViewScreen'
 
 
-const SinglePage = () => (
-    <SafeAreaView>
-    <Card>
-        <Card.Title title="Card Title" subtitle="Card Subtitle" />
-        <Card.Content>
-            <Title>Card title</Title>
-            <Paragraph>Card content</Paragraph>
-        </Card.Content>
-            
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-        </Card.Actions>
-    </Card>
-     </SafeAreaView>
-);
+const SinglePage = ({navigation, route, item}) =>
+{
+    const { id, apartment_img1,
+        apartment_describ, apartment_location,
+        apartment_price, rent_period,
+        apartment_img2, apartment_img3,
+        apartment_img4, apartment_img5
+    } = route.params;
+    
+    return (
+        <View>
+            <SafeAreaView>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}>
+            <Card>
+                <Card.Content>
+                    <Title style={{fontWeight:'bold', marginBottom:20}}>{apartment_describ}</Title>
+                    <Paragraph style={{ fontSize: 18 }}>location: {apartment_location}</Paragraph>
+                    <Paragraph style={{ fontSize: 18 }}>location: {rent_period}</Paragraph>
+                </Card.Content>
+                    
+                <Card.Cover source={{ uri: 'https://easy-apartment14.herokuapp.com' + apartment_img1 }} />
+                        <Card.Content>
+                            <Paragraph style={{ fontSize: 20, fontWeight: 'bold', paddingTop: 10, textAlign: 'center', paddingBottom: 20 }}>Interior View</Paragraph> 
+                            <Divider />
+                            <View style={{flexDirection:'row'}}>
+                                <Card.Cover source={{ uri: 'https://easy-apartment14.herokuapp.com' + apartment_img2 }} style={{width:'45%', marginTop:20, marginRight:'10%'}} />
+                                <Card.Cover source={{ uri: 'https://easy-apartment14.herokuapp.com' + apartment_img3 }} style={{width:'45%', marginTop:20}} />
+                            </View>
+                            <View style={{flexDirection:'row'}}>
+                                <Card.Cover source={{ uri: 'https://easy-apartment14.herokuapp.com' + apartment_img5 }} style={{width:'45%', marginTop:20, marginRight:'10%'}} />
+                                <Card.Cover source={{ uri: 'https://easy-apartment14.herokuapp.com' + apartment_img4 }} style={{width:'45%', marginTop:20, marginBottom:20}} />
+                            </View>
+                            <Paragraph style={{ fontSize: 20, fontWeight:'400', paddingTop:10, textAlign:'center' }}>Book This Appartment for ${ apartment_price}</Paragraph> 
+                        </Card.Content>
+
+                        <Card.Actions>
+                            <Button onPress={()=> navigation.navigate('ViewScreen')}>Continue to website</Button>
+                        </Card.Actions>
+                        
+                    </Card>
+                    </ScrollView>
+             </SafeAreaView>
+        </View>
+        );
+}
+
 
 export default SinglePage;
